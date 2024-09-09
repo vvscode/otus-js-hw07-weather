5dc559dc52f23d892593847ab01a26d46c2b573e
av <av@mcc.vniiem.ru>
2024-09-09 20:03:23 +0300

    install prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import jest from "eslint-plugin-jest";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
@@ -19,4 +20,5 @@ export default [
       "no-undef": "error",
     },
   },
+  eslintConfigPrettier,
 ];
