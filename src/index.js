6cc06f92133d3fa3e353f55d13321b89062ba1fa
av <av@mcc.vniiem.ru>
2024-09-10 18:31:36 +0300

    simple html and css markup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
-document.querySelector(".app").innerHTML = `<h1>a + b = ${sum(1, 2)}</h1>`;
+document.querySelector(".app").innerHTML = `
+      <span>
+        <div class="panel-search">
+          <input
+            class="input-search"
+            type="search"
+            inputmode="latin"
+            required
+            placeholder="Moscow"
+          />
+          <button class="btn-search">Show</button>
+        </div>
+        <div>
+          <span>
+            <img class="city-map" src="map.png" />
+          </span>
+          <span>
+            <textarea class="weather-info">Weather info</textarea>
+          </span>
+        </div>
+      </span>
+      <span class="sidebar">
+        <label class="history-label">HISTORY</label>
+        <ul class="search-history">
+          <li>Moscow</li>
+          <li>Minsk</li>
+          <li>London</li>
+        </ul>
+      </span>
+`;
 
 export function sum(a, b) {
   return a + b;
