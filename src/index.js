221c1dc783575eb58df4cef5e80bfdf33b58e27f
drugpamir <toll_aa@mail.ru>
2024-09-15 23:13:15 +0300

    Application start moved to another script file (runApp.js)

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
-document.querySelector(".app").innerHTML = `
-      <span>
-        <div class="panel-search">
-          <input
-            class="input-search"
-            type="search"
-            inputmode="latin"
-            required
-            placeholder="Moscow"
-          />
-          <button class="btn-search">Show</button>
-        </div>
-        <div>
-          <span>
-            <img class="city-map" src="map.png" />
-          </span>
-          <span>
-            <textarea class="weather-info">Weather info</textarea>
-          </span>
-        </div>
-      </span>
-      <span class="sidebar">
-        <label class="history-label">HISTORY</label>
-        <ul class="search-history">
-          <li>Moscow</li>
-          <li>Minsk</li>
-          <li>London</li>
-        </ul>
-      </span>
-`;
+import runApp from "./runApp";
+
+runApp(document.querySelector(".app"));
---
+++ src/runApp.js
@@ -0,0 +1,36 @@
+/**
+ * Запуск приложения
+ * @param {Element} el - Корневой элемент в теле разметки главной страницы
+ */
+export default function runApp(el) {
+  el.innerHTML = `
+      <span>
+        <div class="panel-search">
+          <input
+            class="input-search"
+            type="search"
+            inputmode="latin"
+            required
+            placeholder="Moscow"
+          />
+          <button class="btn-search">Show</button>
+        </div>
+        <div>
+          <span>
+            <img class="city-map" src="map.png" />
+          </span>
+          <span>
+            <textarea class="weather-info">Weather info</textarea>
+          </span>
+        </div>
+      </span>
+      <span class="sidebar">
+        <label class="history-label">HISTORY</label>
+        <ul class="search-history">
+          <li>Moscow</li>
+          <li>Minsk</li>
+          <li>London</li>
+        </ul>
+      </span>
+`;
+}
