5489506f379319043245e0385c32bc57924b06e4
av <av@mcc.vniiem.ru>
2024-09-16 20:11:40 +0300

    test fetch

--- src/runApp.js
+++ src/runApp.js
@@ -1,10 +1,12 @@
 import mapImg from "./map.png";
 
+const apiKey = "7881bfb7be02c74633e5fdee4ff41329";
+
 /**
  * Запуск приложения
  * @param {Element} el - Корневой элемент в теле разметки главной страницы
  */
-export default function runApp(el) {
+export default async function runApp(el) {
   el.innerHTML = `
       <span>
         <div class="panel-search">
@@ -35,4 +37,23 @@ export default function runApp(el) {
         </ul>
       </span>
 `;
+
+  await getCurrentWeaherByCityName("Moscow");
+  // await getWeaherForecastByCityName();
+}
+
+async function getCurrentWeaherByCityName(cityName) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${apiKey}`;
+  console.log(url);
+  const result = await fetch(url);
+  console.log(result);
+  console.log(await result.json());
+}
+
+async function getWeaherForecastByCityName() {
+  const url = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=37.61&lon=55.75&appid=${apiKey}`;
+  console.log(url);
+  const result = await fetch(url);
+  console.log(result);
+  console.log(await result.json());
 }
