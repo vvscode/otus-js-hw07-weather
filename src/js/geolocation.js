732cef440f2bc65c353043b6549c6cab5257a478
av <av@mcc.vniiem.ru>
2024-09-17 18:14:26 +0300

    added geolocation module

---
+++ src/js/geolocation.js
@@ -0,0 +1,27 @@
+let coords;
+
+const options = {
+  enableHighAccuracy: true,
+  maximumAge: 30000,
+  timeout: 10000,
+};
+
+/**
+ * Возвращает текущие координаты из браузера (Geolocation API)
+ * @returns {Array.<number>} - Массив [широта, долгота]
+ */
+export async function fetchCoords() {
+  if (navigator.geolocation) {
+    const promise = new Promise((resolve, reject) =>
+      navigator.geolocation.getCurrentPosition(resolve, reject)
+    );
+
+    const nav = await promise;
+    coords = [nav.coords.latitude, nav.coords.longitude];
+  }
+  return coords;
+}
+
+export function hasCoords() {
+  return coords.every(Number.isFinite);
+}
