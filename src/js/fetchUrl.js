2155339eff5d4efb4a1f95d035989f5e02ae67cf
av <av@mcc.vniiem.ru>
2024-09-30 18:13:21 +0300

    fetchUrl tests

--- src/js/fetchUrl.js
+++ src/js/fetchUrl.js
@@ -11,12 +11,21 @@ export async function fetchJson(
   timeoutMsecs = 5000
 ) {
   try {
-    options.signal = AbortSignal.timeout(timeoutMsecs);
+    if (isAbsoluteURL(url)) options.signal = AbortSignal.timeout(timeoutMsecs);
     const result = await fetch(url, options);
     if (result.ok) {
       return result.json();
     }
   } catch (e) {
-    console.log(e);
+    console.log("fetch error: ", e);
   }
+}
+
+/**
+ * Проверка на то, является ли адрес ссылки абсолютным или относительным
+ * @param {string} url - адрес ссылки
+ * @returns {boolean} True = абсолютный, False = относительный
+ */
+export function isAbsoluteURL(url) {
+  return url.indexOf("://") > 0 || url.indexOf("//") === 0;
 }
---
+++ src/js/weatherApi.json.js
@@ -0,0 +1,46 @@
+export const weather = {
+  coord: {
+    lon: 37.6156,
+    lat: 55.7522,
+  },
+  weather: [
+    {
+      id: 804,
+      main: "Clouds",
+      description: "overcast clouds",
+      icon: "04d",
+    },
+  ],
+  base: "stations",
+  main: {
+    temp: 19.91,
+    feels_like: 18.9,
+    temp_min: 18.75,
+    temp_max: 20.29,
+    pressure: 1018,
+    humidity: 36,
+    sea_level: 1018,
+    grnd_level: 999,
+  },
+  visibility: 10000,
+  wind: {
+    speed: 5.5,
+    deg: 181,
+    gust: 11.07,
+  },
+  clouds: {
+    all: 99,
+  },
+  dt: 1727362291,
+  sys: {
+    type: 2,
+    id: 2095214,
+    country: "RU",
+    sunrise: 1727320975,
+    sunset: 1727363918,
+  },
+  timezone: 10800,
+  id: 524901,
+  name: "Moscow",
+  cod: 200,
+};
